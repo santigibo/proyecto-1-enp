65b2fa8fa9ddc8bc2083b2fec1471622ba0c4af5
Santiago Giraldo Botero <santigibo@hotmail.com>
2021-10-02 12:34:30 -0500

    Change comments from english to spanish

--- results.r
+++ results.r
@@ -5,20 +5,20 @@ m  = 5000
 n = 2000
 L = 2000
 
-# First part to calculate statistic under H_0 hypothesis
+# Primera parte para caclular bajo la hipótesis nula H_0
 
-ugestad = u_statistic(m,n,L) # 500 array of independent u_statistics
+ugestad = u_statistic(m,n,L) # Vector de los 500 valores independientes
 mean_1 = mean(ugestad)
 sd_1 = sd(ugestad)
 
 cvm_test = cvm.test(ugestad, "pnorm", mean=1, sd=0) # Cramér-Von Mises test
 
-per = percentage(ugestad, 1.96) # Percentage of data outside percentile
+per = percentage(ugestad, 1.96) # Porcentaje de datos afuera del percentil 95
 
-# Second part to calculate statistic under alternative
+# Segunda parte para calcular bajo la hipótesis alternativa
 
-ugestad2 = u_statistic2(m,n,L) # 500 array of independent u_statistics
+ugestad2 = u_statistic2(m,n,L) # Vector de los 500 valores independientes
 mean_2 = mean(ugestad2)
 sd_2 = sd(ugestad2)
 
-per2 = percentage(ugestad2, 1.96) # Percentage of data outside percentile
+per2 = percentage(ugestad2, 1.96) # Porcentaje de datos afuera del percentil 95
